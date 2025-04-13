40fabbebd593c029a7968e4e6e03cf9d79cbc188
Yassin Chamlal <64170166+yassi22@users.noreply.github.com>
2025-04-13 18:56:56 +0200

    added html, fixed errors

--- yassin-portfolio/src/app/app.component.ts
+++ yassin-portfolio/src/app/app.component.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule,RouterOutlet], 
+  standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'yassin-portfolio';
+  title = 'yassin-portfolio'; 
+  isDarkMode = false;
+  lang = 'nl';
+
+  toggleDarkMode() {
+    this.isDarkMode = !this.isDarkMode;
+    document.body.classList.toggle('dark-mode', this.isDarkMode);
+  }
+
+  switchLang() {
+    this.lang = this.lang === 'nl' ? 'en' : 'nl';
+  }
 }
 
  
--- yassin-portfolio/src/app/app.routes.ts
+++ yassin-portfolio/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { SkillsComponent } from './skills/skills.component';
+import { CvComponent } from './cv/cv.component';
+import { ContactComponent } from './contact/contact.component';
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'projecten', component: ProjectsComponent },
+  { path: 'skills', component: SkillsComponent },
+  { path: 'cv', component: CvComponent },
+  { path: 'contact', component: ContactComponent }
+];
+
