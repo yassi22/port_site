97eb701dfa766ad541f3f45bc514628b671b5cc1
Yassin Chamlal <64170166+yassi22@users.noreply.github.com>
2025-04-14 09:41:25 +0200

    fixed routing

--- yassin-portfolio/src/app/home/home.component.ts
+++ yassin-portfolio/src/app/home/home.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'; 
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
